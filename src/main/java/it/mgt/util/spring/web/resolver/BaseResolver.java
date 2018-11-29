33bba4519aadb8355635c26976ce153a4e020d65
Michele Tibaldi <michele.tibaldi@holonix.it>
2018-11-29 15:39:08 +0100

    Method parseParam has been moved to JPA utils.

--- src/main/java/it/mgt/util/spring/web/resolver/BaseResolver.java
+++ src/main/java/it/mgt/util/spring/web/resolver/BaseResolver.java
@@ -13,56 +13,6 @@ import java.util.stream.Collectors;
 
 public abstract class BaseResolver implements HandlerMethodArgumentResolver {
 
-    protected Object parseParam(String value, Class<?> type) {
-        Object result = value;
-
-        if (type != null) {
-            if (Boolean.class.isAssignableFrom(type)) {
-                result = Boolean.parseBoolean(value);
-            }
-            else if (Byte.class.isAssignableFrom(type)) {
-                result = Byte.parseByte(value);
-            }
-            else if (Short.class.isAssignableFrom(type)) {
-                result = Short.parseShort(value);
-            }
-            else if (Integer.class.isAssignableFrom(type)) {
-                result = Integer.parseInt(value);
-            }
-            else if (Long.class.isAssignableFrom(type)) {
-                result = Long.parseLong(value);
-            }
-            else if (Float.class.isAssignableFrom(type)) {
-                result = Float.parseFloat(value);
-            }
-            else if (Double.class.isAssignableFrom(type)) {
-                result = Double.parseDouble(value);
-            }
-            else if (BigInteger.class.isAssignableFrom(type)) {
-                result = new BigInteger(value);
-            }
-            else if (BigDecimal.class.isAssignableFrom(type)) {
-                result = new BigDecimal(value);
-            }
-            else if (Date.class.isAssignableFrom(type)) {
-                result = new Date(Long.parseLong(value));
-            }
-            else if (UUID.class.isAssignableFrom(type)) {
-                result = UUID.fromString(value);
-            }
-            else if (Enum.class.isAssignableFrom(type))  {
-                try {
-                    Method valueOf = type.getMethod("valueOf", String.class);
-                    result = valueOf.invoke(null, value);
-                }
-                catch (Exception ignored) {
-                }
-            }
-        }
-
-        return result;
-    }
-
     protected Map<String, List<String>> getParameters(NativeWebRequest nativeWebRequest, PathParam[] pathParams, QueryParam[] queryParams) {
         HttpServletRequest httpServletRequest = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
         Map<String, String> pathVariables = (Map<String, String>) httpServletRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
