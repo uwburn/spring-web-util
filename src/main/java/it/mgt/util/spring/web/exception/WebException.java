2ac54084992d24dc44a2c3d91f1ec0241e00dafb
Michele Tibaldi <tiboburn@gmail.com>
2020-05-19 14:49:06 +0200

    Set exception message

--- src/main/java/it/mgt/util/spring/web/exception/WebException.java
+++ src/main/java/it/mgt/util/spring/web/exception/WebException.java
@@ -11,33 +11,40 @@ public abstract class WebException extends RuntimeException {
     public WebException() { }
 
     public WebException(int status) {
+        super("(" + status + ")");
         this.status = status;
     }
 
     public WebException(int status, String code, String message) {
+        super("(" + status + ") " + code + " - " + message);
         this.status = status;
         payload = new ExceptionPayload(code, message);
     }
 
     public WebException(int status, ErrorDefinition errorDefinition) {
+        super("(" + status + ") " + errorDefinition.getCode() + " - " + errorDefinition.getMessage());
         this.status = status;
         payload = new ExceptionPayload(errorDefinition.getCode(), errorDefinition.getMessage());
     }
 
     public WebException(String code, String message) {
+        super(code + " - " + message);
         payload = new ExceptionPayload(code, message);
     }
 
     public WebException(ErrorDefinition errorDefinition) {
+        super(errorDefinition.getCode() + " - " + errorDefinition.getMessage());
         payload = new ExceptionPayload(errorDefinition.getCode(), errorDefinition.getMessage());
     }
 
     public WebException(int status, CodedException codedException) {
+        super("(" + status + ") " + codedException.getCode() + " - " + codedException.getMessage());
         this.status = status;
         payload = new ExceptionPayload(codedException.getCode(), codedException.getMessage());
     }
 
     public WebException(int status, Object payload) {
+        super("(" + status + ")");
         this.status = status;
         this.payload = payload;
     }
